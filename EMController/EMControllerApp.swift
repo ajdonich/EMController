1535780a72e8555742322ba0ce3516a55f048de5
AJ Donich <ajdonich@gmail.com>
2023-11-27 13:04:29 -0800

    Initial Commit

---
+++ EMController/EMControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EMControllerApp.swift
+//  EMController
+//
+//  Created by Alfred J Donich on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct EMControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
